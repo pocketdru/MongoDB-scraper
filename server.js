f54036539c5fc94320766e876cfbcff60aed5964
Mary <pocketdru@gmail.com>
2019-03-05 18:29:50 -0700

    commit5

--- server.js
+++ server.js
@@ -37,17 +37,11 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/unit18Populate
 
 mongoose.connect(MONGODB_URI);
 
-
-// mongoose.connect("mongodb://localhost/", {
-//   useNewUrlParser: true
-// });
-
 app.get("/", function (req, res) {
 
   axios.get("https://www.nytimes.com/").then(function (response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
-    // console.log(cheerio.load(response.length));
     // Now, we grab every h2 within an article tag, and do the following:
     $("article").each(function (i, element) {
       // Save an empty result object
@@ -69,8 +63,6 @@ app.get("/", function (req, res) {
           console.log(err);
         });
     });
-    // Send a message to the client
-    // res.send("Scrape Complete");
 
   });
   db.Article.find({})
@@ -84,40 +76,6 @@ app.get("/", function (req, res) {
       res.json(err);
     })
 });
-
-// app.get("/scrape", function (req, res) {
-//   // First, we grab the body of the html with axios
-//   axios.get("https://www.nytimes.com/").then(function (response) {
-//     // Then, we load that into cheerio and save it to $ for a shorthand selector
-//     var $ = cheerio.load(response.data);
-//     // console.log(cheerio.load(response.length));
-//     // Now, we grab every h2 within an article tag, and do the following:
-//     $("article").each(function (i, element) {
-//       // Save an empty result object
-//       var result = {};
-
-//       // Add the text and href of every link, and save them as properties of the result object
-//       result.title = $(element).text();
-//       result.body = $(element).find("p").text();
-//       result.link = "https://www.nytimes.com" + $(element).find("a").attr("href");
-
-//       // Create a new Article using the `result` object built from scraping
-//       db.Article.create(result)
-//         .then(function (dbArticle) {
-//           // View the added result in the console
-//           console.log(dbArticle);
-//         })
-//         .catch(function (err) {
-//           // If an error occurred, log it
-//           console.log(err);
-//         });
-//     });
-
-//     // Send a message to the client
-//     res.send("Scrape Complete");
-
-//   });
-// });
 
 // Route for getting all Articles from the db
 app.get("/articles", function (req, res) {
@@ -143,18 +101,6 @@ app.get("/articles/:id", function (req, res) {
       res.json(err);
     });
 });
-
-// // Route for saving/updating an Article's associated Note
-// app.post("/articles/:id", function (req, res) {
-//   db.Note.create(req.body)
-//     .then(function (dbNote) {
-//       return db.Article.findByIdAndUpdate({
-//         _id: req.params.id
-//       }, {
-//         new: true
-//       });
-//     })
-// });
 
 app.get("/saved", function (req, res) {
   db.Saved.find({})
@@ -202,8 +148,6 @@ app.get("/savedView", function (req, res) {
       res.render("savedView", {
         dbSaved
       });
-      // console.log(dbArticle[1]._id);
-      // console.log(dbArticle.length);
     })
     .catch(function (err) {
       res.json(err);
